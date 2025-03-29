f1f975ee735f9509c6fb81bc86a1996807e6f1ff
Santiago Lozano <71517748+santiagolc02@users.noreply.github.com>
2025-03-28 23:46:16 -0600

    created Login.jsx

---
+++ front/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex h-screen w-screen items-center justify-center'>
+        <h1>Proyecto final</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
