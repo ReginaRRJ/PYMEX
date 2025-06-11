c161608a497cc417e1287a1ce0bf23e59d3f82db
carolinasntos <111061645+carolinasntos@users.noreply.github.com>
2025-06-11 19:52:57 +0000

    :D

--- front/vite.config.js
+++ front/vite.config.js
@@ -6,7 +6,30 @@ import { createRequire } from 'module'
 
 const require = createRequire(import.meta.url)
 
+/*export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      process: require.resolve('process/browser'),
+      stream: require.resolve('stream-browserify'),
+      util: require.resolve('util'),
+    },
+  },
+  define: {
+    'process.env': {}, // evita errores de process.env
+  },
+  server: {
+    proxy: {
+      '/login': 'http://localhost:3000',
+      '/users': 'http://localhost:3000',
+    },
+  },
+});*/
+
+
+// 👇 Aquí agregamos base: '/PYMEX/'
 export default defineConfig({
+  base: '/PYMEX/', // 👈 Ajusta esto al nombre exacto de tu repo
   plugins: [react()],
   resolve: {
     alias: {
