ac602b1ac04a84bc8396e8f9d47087f0b167d7e7
nancysilva <A00833627@tec.mx>
2025-06-10 22:55:31 -0600

    Coverage Sonnar

--- front/src/test/ReportesUsuario.test.jsx
+++ front/src/test/ReportesUsuario.test.jsx
@@ -1,3 +1,4 @@
+//ReportesUsuario.test.jsx
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import ReportesUsuarios from '../pages/Admin/ReportesUsuarios';
